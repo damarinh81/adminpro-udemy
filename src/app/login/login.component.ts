94cab913e6e82d1a969982a76ac28ffd9df7eeb8
danny <damarinh81@gmail.com>
2019-08-09 08:51:07 -0500

    inicio de proyecto

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute,Params } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+  redirigir()
+  {
+    this._router.navigate(['/register']);
+  }
+}
